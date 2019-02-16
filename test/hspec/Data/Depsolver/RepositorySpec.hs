4ba2eadfde2469cba81ec4c5b9fd4c23195b4b97
Ben Moon <software@guiltydolphin.com>
2019-02-16 21:55:44 +0000

    Add tests for nested dependencies

--- test/hspec/Data/Depsolver/RepositorySpec.hs
+++ test/hspec/Data/Depsolver/RepositorySpec.hs
@@ -49,8 +49,17 @@ spec = do
             in (repo, repoState) `shouldNotSatisfy` uncurry validState
          it "a state is not valid if it contains a package name and version that is not in the repository" $
             propStateInvalid1 withoutPackage repoStateWithPackageVersion
-         it "a state is not valid if it contains packages with unmet dependencies" $
-            propStateInvalid2 makeDependency (\p1 _ -> repoStateWithPackageVersion p1)
+         context "unmet dependencies" $ do
+                 it "A>>B, [A]" $
+                    propStateInvalid2 makeDependency (\p1 _ -> repoStateWithPackageVersion p1)
+                 it "A>>B, B>>C, [A, B]" $
+                    propStateInvalid3 (\p1 p2 p3 -> do
+                                         makeDependency p1 p2
+                                         makeDependency p2 p3) (\p1 p2 _ -> repoStateWithPackageVersions [p1, p2])
+                 it "A>>B, B>>C, [A, C]" $
+                    propStateInvalid3 (\p1 p2 p3 -> do
+                                         makeDependency p1 p2
+                                         makeDependency p2 p3) (\p1 _ p3 -> repoStateWithPackageVersions [p1, p3])
          it "a state is not valid if it contains conflicting packages" $
             propStateInvalid2 makeConflict repoStateWithPackageVersions2
          it "a state is not valid if it contains a wildcard conflict" $
@@ -59,4 +68,5 @@ spec = do
                 forAll (gen2 (execRepoGen rg, stateGen)) (not . uncurry validState)
             propStateInvalid1 rg stateGen = property $ \p1 -> checkStateInvalid (rg p1) (stateGen p1)
             propStateInvalid2 rg stateGen = property $ \(p1, p2) -> checkStateInvalid (rg p1 p2) (stateGen p1 p2)
+            propStateInvalid3 rg stateGen = property $ \(p1, p2, p3) -> checkStateInvalid (rg p1 p2 p3) (stateGen p1 p2 p3)
             repoStateWithPackageVersions2 p1 p2 = repoStateWithPackageVersions [p1, p2]
