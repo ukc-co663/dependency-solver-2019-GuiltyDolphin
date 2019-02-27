5ff2b37d4ff9146096255d23ad0eae5084cd74c8
Ben Moon <software@guiltydolphin.com>
2019-02-27 11:38:51 +0000

    Add initial CLI
    - currently supports reading in files and solving (with an indication
      of when a solution could not be reached)
    - outputs a JSON list of commands

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,30 @@
 module Main where
 
+
+import System.Environment (getArgs)
+import System.Exit (exitWith, ExitCode(ExitFailure))
+
+import qualified Text.JSON as TJ
+
+import Data.Depsolver.Parse
+    ( parseRepo
+    , parseRepoState
+    , parseConstraints )
+import Data.Depsolver.Solver (solve)
+
+
 main :: IO ()
-main = putStrLn "main is not yet implemented"
+main = do
+  args <- getArgs
+  case args of
+    [repoF, initialF, constrsF] -> do
+        repo <- readAndParse repoF parseRepo "repository"
+        repoState <- readAndParse initialF parseRepoState "initial state"
+        constraints <- readAndParse constrsF parseConstraints "constraints"
+        let solved = solve repo constraints repoState
+        case solved of
+          Nothing -> exitErr "couldn't solve constraints"
+          Just (_, cmds) -> putStrLn $ TJ.encodeStrict cmds
+    _ -> exitErr "usage: ./solve REPO INITIAL-STATE CONSTRAINTS"
+    where exitErr s = putStrLn s >> exitWith (ExitFailure 1)
+          readAndParse f p e = readFile f >>= maybe (exitErr $ "could not parse " <> e) pure . p
